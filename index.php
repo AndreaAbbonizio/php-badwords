31d49ac87232211a153a4c818c288f5bdd80e0bf
AndreaAbbonizio <andreaabbonizio01@gmail.com>
2023-04-17 16:28:00 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BadWords</title>
+</head>
+<body>
+<h1>BadWords</h1>
+<p>inserisci un testo e una parola da censurare</p>
+<form action="resultPage.php" method="GET">
+    <input type="text" name="text" placeholder="Inserisci un testo">
+
+    <br>
+
+    <input type="text" name="word" placeholder="Inserisci una parola">
+
+    <input type="submit">
+</form>
+
+    
+</body>
+</html>
